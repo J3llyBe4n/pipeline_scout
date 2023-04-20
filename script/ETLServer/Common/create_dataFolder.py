01b1a941da1ffd93ba9553652eb0a83992d12026
hooniegit <neivekim76@gmail.com>
2023-04-20 16:57:24 +0900

    test

--- script/ETLServer/Common/create_dataFolder.py
+++ script/ETLServer/Common/create_dataFolder.py
@@ -3,15 +3,15 @@ import datetime
 import json
 
 nowDate = datetime.datetime.now().date().strftime("%Y_%m_%d")
+directory = os.path.join(os.path.dirname(__file__), '..', '..', '..', 'datas', 'Logs')
 
 def createFolder():
-    os.mkdir(nowDate)
+    os.mkdir("%s/%s" %(directory, nowDate))
     print("folder created")
-
 
 def createJsonFile():
     data ={}
-    with open("%s/%s.json" %(nowDate, nowDate), "w") as json_file:
+    with open("%s/%s/%s.json" %(directory, nowDate, nowDate), "w") as json_file:
         json.dump(data, json_file, indent=4)
 
     print("file created")
