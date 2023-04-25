778e5047b5b2739e443412ea9cc04c0bd5559c92
woorek <lkjwj95@gmail.com>
2023-04-25 15:31:58 +0900

    teamstats added

---
+++ ETLServer/DataLakes/DL_Tstats_main.py
@@ -0,0 +1,28 @@
+from ETLServer.Modules.db_function import *
+from ETLServer.Modules.DL_api_function import *
+
+def get_GmtNowDate():
+    tmp_GmtNowDate = datetime.datetime.utcnow().date()
+    Gmt_NowDate = (tmp_GmtNowDate-datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+
+    return Gmt_NowDate
+
+round_Date = get_GmtNowDate()
+
+api_keys = 'a86d420d0d8840c8e722e16cf9742f7b'
+
+db_func = DBfunc()
+
+apiFunc = ApiTeamStatistics()
+
+#db 서버 열어주기
+db_func.connect_SQL()
+
+tmp_leagueId = db_func.read_leagueId()
+
+tmp_teamId = db_func.read_teamId()
+
+round_Date = db_func.read_roundDate()
+
+#json 파일 로드 하고 로컬에 관리해주기
+apiFunc.Api_TstatsJson(tmp_leagueId, tmp_teamId, api_keys, round_Date)
---
+++ ETLServer/create_TstatsFolder.py
@@ -0,0 +1,44 @@
+import os
+import datetime
+import json
+from ETLServer.Modules.db_function import *
+
+directory = os.path.join(os.path.dirname(__file__), 'datas', 'DataLake')
+nowDate = datetime.datetime.now().date().strftime("%Y_%m_%d")
+
+def create_teamsFloder():
+	if not os.path.exists("%s/teams" % directory):
+        os.mkdir("%s/teams" % directory)
+        print("folder created")
+    else:
+        print("already exists!")
+
+def create_teamsStatisticsFolder():
+    if not os.path.exists("%s/teams/statistics" % directory):
+        os.mkdir("%s/teams/statistics" % directory)
+        print("folder created")
+    else:
+        print("already exists!")
+
+def create_teamStatisticsJson():
+    db_func = DBfunc()
+
+    #DB server연결 
+    db_func.connectSQLServer()
+
+    #DB league_id 리스트 반환 
+    tmp_leagueId = db_func.read_tmpLeagueId()
+
+    print(tmp_leagueId)
+
+    for i in tmp_leagueId:
+        leagueId = i 
+        file_path = "%s/teams/statistics/%s_%s_Tstats.json" % (directory, nowDate,leagueId)
+        data = {'data' : []}
+        
+        if os.path.exists(file_path):
+            print("file exists")
+        
+        else:
+            with open(file_path, "w") as json_file:
+                json.dump(data, json_file, indent=4)
