9514969737d9ad83466a7c3ad7025d66fc088039
hooniegit <neivekim76@gmail.com>
2023-04-28 17:50:56 +0900

    load fixed

--- ETLServer/Modules/load_toLocalJson.py
+++ ETLServer/Modules/load_toLocalJson.py
@@ -12,255 +12,313 @@ import json
 import os
 from datetime import datetime
 
+# standing - 매일
+# [년월일]_standings.json
 
-def load_standingJson(tmp_data, league_id):
-    now_date = datetime.utcnow().date().strftime("%Y_%m_%d")
-    
-    directory = os.path.join(os.path.dirname(__file__), "..", 'datas', 'DataLake', 'standings')
-    #print(directory)
+def load_standingJson(tmp_data): #
 
-    # test 하려고 이 부분을 추가(현재 폴더 싸개가 없으므로..?)
-    try :
-        with open("%s/%s_%d_standing.json" % (directory, now_date, league_id), "r") as json_file:
-            data = json.load(json_file)
-    except:
-        with open("%s/%s_%d_standing.json" % (directory, now_date, league_id), "w") as json_file:
-            data = {'data' : []}
-            json.dump(data, json_file, indent=4)
+    now_year = datetime.utcnow().date().strftime("%Y") #
+    now_date = datetime.utcnow().date().strftime("%y_%m_%d") #
+    directory = os.path.join(os.path.dirname(__file__), "..", 'datas', 'DataLake', 'standings', now_year) #
 
-    # with open("%s/%s_%d_standing.json" % (directory, now_date, league_id), "r") as json_file:
-    #     data = json.load(json_file)
-     
+    with open("%s/%s_standing.json" % (directory, now_date), "r") as json_file: #
+        data = json.load(json_file)
+
     data['data'].append(tmp_data)
 
-    print(tmp_data)
-
-    with open("%s/%s_%d_standing.json" % (directory, now_date, league_id), "w") as json_file:
+    with open("%s/%s_standing.json" % (directory, now_date), "w") as json_file: #
         json.dump(data, json_file, indent=4)
-        print("sucksex")
+        print("Load is done!")
 
-def load_fixtureJson(tmp_data, league_id):
-    now_date = datetime.utcnow().date().strftime("%y%m%d")
-    
-    directory = os.path.join(os.path.dirname(__file__), "..", 'datas', 'DataLake', 'fixtures', 'fixtures')
-    #print(directory)
 
-    with open("%s/%s_%d_fixture.json" % (directory, now_date, league_id), "r") as json_file:
+
+# fixture/fixture - 1년
+# 시즌(22)_[리그 ID]_fixture.json
+# 변수로 리그ID 필요
+
+def load_fixtureJson(tmp_data, league_id): #
+
+    now_year = datetime.utcnow().date().strftime("%Y") #
+    directory = os.path.join(os.path.dirname(__file__), "..", 'datas', 'DataLake', 'fixtures', 'fixtures', now_year) #
+
+    with open("%s/%s_%d_fixture.json" % (directory, str(now_year)[2:], league_id), "r") as json_file:
         data = json.load(json_file)
      
     data['data'].append(tmp_data)
 
     print(tmp_data)
 
-    with open("%s/%s_%d_fixture.json" % (directory, now_date, league_id), "w") as json_file:
+    with open("%s/%s_%d_fixture.json" % (directory, now_year, league_id), "w") as json_file:
         json.dump(data, json_file, indent=4)
-        print("sucksex")
+        print("Load is done!")
 
-def load_TstatsJsonData(tmp_data, league_id):
-    now_date = datetime.utcnow().date().strftime("%y%m%d")
-    
-    directory = os.path.join(os.path.dirname(__file__), "..", 'datas', 'DataLake', 'teams', 'statistics')
-    with open("%s/teams/statistics/%s/%s_%s_Tstats.json" % (directory, now_date, now_date, league_id), "r") as json_file:
+
+
+#################################################33
+# teams/statistics - 매일
+# [리그 ID]_Tstats.json
+# 변수로 리그ID 필요
+
+def load_TstatsJsonData(tmp_data, league_id): #
+
+    now_year = datetime.utcnow().date().strftime("%Y") #    
+    directory = os.path.join(os.path.dirname(__file__), "..", 'datas', 'DataLake', 'teams', 'Statistics', now_year) #
+
+    with open("%s/%s_Tstats.json" % (directory, league_id), "r") as json_file: #
         data = json.load(json_file)
      
     data['data'].append(tmp_data)
 
     # print(tmp_data)
 
-    with open("%s/teams/statistics/%s/%s_%s_Tstats.json" % (directory, now_date, now_date, league_id), "w") as json_file:
+    with open("%s/%s_Tstats.json" % (directory, league_id), "w") as json_file: #
         json.dump(data, json_file, indent=4)
-        print(league_id)
-        print("sucksex")
+        print("Load is done!")
 
-def load_TinfosJson(tmp_data, league_id):
-    now_date = datetime.utcnow().date().strftime("%y%m%d")
-    
-    directory = os.path.join(os.path.dirname(__file__), "..", 'datas', 'DataLake', 'teams', 'teams_info')
-    #print(directory)
 
-    try :
-        with open("%s/%s_%d_Tinfo.json" % (directory, now_date, league_id), "r") as json_file:
-            data = json.load(json_file)
-    except:
-        with open("%s/%s_%d_Tinfo.json" % (directory, now_date, league_id), "w") as json_file:
-            data = {'data' : []}
-            json.dump(data, json_file, indent=4)
 
-    data['data'].append(tmp_data)
+# teams/information - 1년
+# [리그 ID]_Tinfo.json
+# 변수로 리그ID 필요
 
-    print(tmp_data)
+def load_TinfosJson(tmp_data, league_id): #
 
-    with open("%s/%s_%d_Tinfo.json" % (directory, now_date, league_id), "w") as json_file:
-        json.dump(data, json_file, indent=4)
-        print("sucksex")
+    now_year = datetime.utcnow().date().strftime("%Y") #
+    directory = os.path.join(os.path.dirname(__file__), "..", 'datas', 'DataLake', 'teams', 'teams_info', now_year) #
 
-def load_h2hJson(tmp_data):
-    
-    date_time = tmp_data[0]['fixture']['date'][:tmp_data[0]['fixture']['date'].index('T')].replace('-','')[2:]
-    print(date_time)
-    league_id = tmp_data[0]['league']['id']
-    print(league_id)
-    
-    directory = os.path.join(os.path.dirname(__file__), "..", 'datas', 'DataLake', 'fixtures', 'h2h')
-
-    with open("%s/%s/%s_%s_h2h.json" %(directory,date_time, date_time, league_id), "r") as json_file:
+    with open("%s/%d_Tinfo.json" % (directory, league_id), "r") as json_file: #
         data = json.load(json_file)
 
     data['data'].append(tmp_data)
 
-    with open("%s/%s/%s_%s_h2h.json" %(directory,date_time, date_time, league_id), "w") as json_file:
+    with open("%s/%d_Tinfo.json" % (directory, league_id), "w") as json_file: #
         json.dump(data, json_file, indent=4)
-        print("heheh")
-    
+        print("Load is done!")
+
+
+
+# fixtures/head to head - 매일
+# [년월일]_h2h.json
+
+def load_h2hJson(tmp_data): #
+
+    now_year = datetime.utcnow().date().strftime("%Y") #
+    now_date = datetime.utcnow().date().strftime("%y%m%d") #
+    directory = os.path.join(os.path.dirname(__file__), "..", 'datas', 'DataLake', 'fixtures', 'H2h', now_year) #
+
+    with open("%s/%s_%s_h2h.json" %(directory, now_date), "r") as json_file: #
+        data = json.load(json_file)
+
+    data['data'].append(tmp_data)
+
+    with open("%s/%s_%s_h2h.json" %(directory, now_date), "w") as json_file: #
+        json.dump(data, json_file, indent=4)
+        print("Load is done!")
+
+
+
+# fixtures/events - 매일
+# [년월일]_events.json
+
 def load_eventsJson(tmp_data):
     
-    now_date = datetime.now().date().strftime("%y%m%d")
-    directory = os.path.join(os.path.dirname(__file__), "..", 'datas/DataLake/fixtures/events')
+    now_year = datetime.utcnow().date().strftime("%Y") #
+    now_date = datetime.utcnow().date().strftime("%y%m%d") #
+    directory = os.path.join(os.path.dirname(__file__), "..", 'datas', 'DataLake', 'fixtures', 'events', now_year) #
 
-    with open("%s/%s/%s_events.json" %(directory, now_date, now_date), "r") as json_file:
+    with open("%s/%s_events.json" %(directory, now_date), "r") as json_file: #
         data = json.load(json_file)
 
     data['data'].append(tmp_data)
 
-    with open("%s/%s/%s_events.json" %(directory, now_date, now_date), "w") as json_file:
+    with open("%s/%s_events.json" %(directory, now_date), "w") as json_file: #
         json.dump(data, json_file, indent=4)
-        print("heheh")
+        print("Load is done!")
 
-def load_fixtureTStatsJsonData(tmp_data):
-    now_date = datetime.utcnow().date().strftime("%y%m%d")
 
-    directory = os.path.join(os.path.dirname(__file__), "..", 'datas', 'DataLake', 'fixtures', 'Tstatistics')
-    with open("%s/%s/%s_Tstatistics.json" % (directory, now_date, now_date), "r") as json_file:
-        data = json.load(json_file)
-     
-    data['data'].append(tmp_data)
+################################################################
+# fixtures/team statistics - 매일
+# [리그 ID]_Ftstats.json
 
-    # print(tmp_data)
+def load_fixtureTStatsJsonData(tmp_data, league_id):
 
-    with open("%s/%s/%s_Tstatistics.json" % (directory, now_date, now_date), "w") as json_file:
-        json.dump(data, json_file, indent=4)
-        print("sucksex")
+    now_year = datetime.utcnow().date().strftime("%Y") #
+    directory = os.path.join(os.path.dirname(__file__), "..", 'datas', 'DataLake', 'fixtures', 'Tstatistics', now_year) #
 
-def load_fixturePStatsJsonData(tmp_data):
-    now_date = datetime.utcnow().date().strftime("%y%m%d")
-
-    directory = os.path.join(os.path.dirname(__file__), "..", 'datas', 'DataLake', 'fixtures', 'Pstatistics')
-    with open("%s/%s/%s_Pstatistics.json" % (directory, now_date, now_date), "r") as json_file:
+    with open("%s/%s_Ftstats.json" % (directory, league_id), "r") as json_file: #
         data = json.load(json_file)
      
     data['data'].append(tmp_data)
 
     # print(tmp_data)
 
-    with open("%s/%s/%s_Pstatistics.json" % (directory, now_date, now_date), "w") as json_file:
+    with open("%s/%s_Ftstats.json" % (directory, league_id), "w") as json_file: #
         json.dump(data, json_file, indent=4)
-        print("sucksex")
+        print("Load is done!")
 
-def load_lineUpsJson(tmp_data):
-    now_date = datetime.utcnow().date().strftime("%y%m%d")
-    directory = os.path.join(os.path.dirname(__file__), "..", 'datas/DataLake/fixtures/lineups')
 
-    with open("%s/%s/%s_lineUps.json" %(directory, now_date, now_date), "r") as json_file:
+
+# fixtures/players statistics - 매일
+# [년월일]_Pstatistics.json
+
+def load_fixturePStatsJsonData(tmp_data): #
+
+    now_year = datetime.utcnow().date().strftime("%Y") #
+    now_date = datetime.utcnow().date().strftime("%y%m%d") #
+    directory = os.path.join(os.path.dirname(__file__), "..", 'datas', 'DataLake', 'fixtures', 'Pstatistics', now_year) #
+
+    with open("%s/%s_Pstatistics.json" % (directory, now_date), "r") as json_file: #
+        data = json.load(json_file)
+     
+    data['data'].append(tmp_data)
+
+    # print(tmp_data)
+
+    with open("%s/%s_Pstatistics.json" % (directory, now_date), "w") as json_file: #
+        json.dump(data, json_file, indent=4)
+        print("Load is done!")
+
+
+
+# fixtures/lineups - 매일
+# [년월일]_lineups.json
+
+def load_lineUpsJson(tmp_data): #
+
+    now_year = datetime.utcnow().date().strftime("%Y") #
+    now_date = datetime.utcnow().date().strftime("%y%m%d") #
+    directory = os.path.join(os.path.dirname(__file__), "..", 'datas', 'DataLake', 'fixtures', 'lineups', now_year) #
+
+    with open("%s/%s_lineUps.json" %(directory, now_date), "r") as json_file:
         data = json.laod(json_file)
 
     data['data'].append(tmp_data)
 
-    with open("%s/%s/%s_lineUps.json" % (directory, now_date, now_date), "w") as json_file:
+    with open("%s/%s_lineUps.json" %(directory, now_date), "w") as json_file:
         json.dump(data, json_file, indent=4)
-        print("sucksex")
+        print("Load is done!")
 
+
+
+# leagues - 1년
+# [리그 ID]_leagueInfo.json
 
 def load_leagueJson(tmp_data, league_id):
 
-    now_date = datetime.utcnow().date().strftime("%y%m%d")
+    now_year = datetime.utcnow().date().strftime("%Y") #
+    directory = os.path.join(os.path.dirname(__file__), "..", 'datas', 'DataLake', 'leagues', now_year) #
 
-    directory = os.path.join(os.path.dirname(__file__), "..", 'datas', 'DataLake', 'leagues')
-    #print(directory)
-
-
-    with open("%s/%s_%d_leagues.json" % (directory, now_date, league_id), "r") as json_file:
+    with open("%s/%d_leagueInfo.json" % (directory, league_id), "r") as json_file: #
         data = json.load(json_file)
 
     data['data'].append(tmp_data)
 
     print(tmp_data)
 
-    with open("%s/%s_%d_leagues.json" % (directory, now_date, league_id), "w") as json_file:
+    with open("%s/%d_leagueInfo.json" % (directory, league_id), "w") as json_file: #
         json.dump(data, json_file, indent=4)
-        print("sucksex")
+        print("Load is done!")
 
 
-def load_psquadJson(tmp_data, team_id):
 
-    now_date = datetime.utcnow().date().strftime("%y%m%d")
-    directory = os.path.join(os.path.dirname(__file__), "..", 'datas', 'DataLake', 'players', 'squad')
+# players/squads - 1년
+# [팀 ID]_Psquad.json
 
-    with open("%s/%s_%d_Psquad.json" % (directory, now_date, team_id), "r") as json_file:
+def load_psquadJson(tmp_data, team_id): #
+
+    now_year = datetime.utcnow().date().strftime("%Y") #
+    directory = os.path.join(os.path.dirname(__file__), "..", 'datas', 'DataLake', 'players', 'Squads', now_year) #
+
+    with open("%s/%d_Psquad.json" % (directory, team_id), "r") as json_file: #
         data = json.load(json_file)
 
     data['data'].append(tmp_data)
     print(tmp_data)
 
-    with open("%s/%s_%d_Psquad.json" % (directory, now_date, team_id), "w") as json_file:
+    with open("%s/%d_Psquad.json" % (directory, team_id), "w") as json_file: #
         json.dump(data, json_file, indent=4)
-        print("sucksex")
+        print("Load is done!")
 
-def load_coachsJsonData(tmp_data, league_id):
 
-    now_date = datetime.utcnow().date().strftime("%y%m%d")
-    directory = os.path.join(os.path.dirname(__file__), "..", 'datas', 'DataLake', 'coachs')
 
-    with open("%s/%s/%s_%d_coachs.json" % (directory, now_date, now_date, league_id), "r") as json_file:
+# teams/coach
+
+# def load_coachsJsonData(tmp_data, league_id):
+
+#     now_date = datetime.utcnow().date().strftime("%y%m%d")
+#     directory = os.path.join(os.path.dirname(__file__), "..", 'datas', 'DataLake', 'coachs')
+
+#     with open("%s/%s/%s_%d_coachs.json" % (directory, now_date, now_date, league_id), "r") as json_file:
+#         data = json.load(json_file)
+
+#     data['data'].append(tmp_data)
+#     print(tmp_data)
+
+#     with open("%s/%s/%s_%d_coachs.json" % (directory, now_date, now_date, league_id), "w") as json_file:
+#         json.dump(data, json_file, indent=4)
+#        print("Load is done!")
+
+
+
+# players/players - 1주
+# [리그 ID]_players.json
+
+def load_pplayerJson(tmp_data, league_id): #
+
+    now_year = datetime.utcnow().date().strftime("%Y") #
+    now_week = datetime.utcnow().date().today().isocalendar()[1] #
+    directory = os.path.join(os.path.dirname(__file__), "..", 'datas', 'DataLake', 'players', 'Players', now_year, now_week) #
+
+    with open("%s/%d_players.json" % (directory, league_id), "r") as json_file: #
         data = json.load(json_file)
 
     data['data'].append(tmp_data)
     print(tmp_data)
 
-    with open("%s/%s/%s_%d_coachs.json" % (directory, now_date, now_date, league_id), "w") as json_file:
+    with open("%s/%d_players.json" % (directory, league_id), "w") as json_file: #
         json.dump(data, json_file, indent=4)
-        print("sucksex")
+        print("Load is done!")
 
-def load_pplayerJson(tmp_data, league_id):
 
-    now_date = datetime.utcnow().date().strftime("%y%m%d")
-    directory = os.path.join(os.path.dirname(__file__), "../datas/DataLake/players/players")
 
-    with open("%s/%s_%s_players.json" % (directory, now_date, league_id), "r") as json_file:
-        data = json.load(json_file)
+# players/top scorers - 매일
+# [리그 ID]_Ptopscorers.json
 
-    data['data'].append(tmp_data)
-    print(tmp_data)
-
-    with open("%s/%s_%s_players.json" % (directory, now_date, league_id), "w") as json_file:
-        json.dump(data, json_file, indent=4)
-        print("sucksex")
+Y = 2022
+y = 22
 
 def load_ptopscorersJson(tmp_data, league_id):
 
-    now_date = datetime.utcnow().date().strftime("%y%m%d")
-    directory = os.path.join(os.path.dirname(__file__), "..", 'datas', 'DataLake', 'players', 'topscorers')
+    now_year = datetime.utcnow().date().strftime("%Y")
+    directory = os.path.join(os.path.dirname(__file__), "..", 'datas', 'DataLake', 'players', 'Topscorers', now_year) #
 
-    with open("%s/%s_%d_Ptopscorers.json" % (directory, now_date, league_id), "r") as json_file:
+    with open("%s/%d_Ptopscorers.json" % (directory, league_id), "r") as json_file: #
         data = json.load(json_file)
 
     data['data'].append(tmp_data)
     print(tmp_data)
 
-    with open("%s/%s_%d_Ptopscorers.json" % (directory, now_date, league_id), "w") as json_file:
+    with open("%s/%d_Ptopscorers.json" % (directory, league_id), "w") as json_file: #
         json.dump(data, json_file, indent=4)
-        print("sucksex")
+        print("Load is done!")
+
+
+
+# predictions - 매일
+# [년월일]
 
 def load_predictionsJsonData(tmp_data):
-    now_date = datetime.utcnow().date().strftime("%y%m%d")
+    now_year = datetime.utcnow().date().strftime("%Y") #
+    now_date = datetime.utcnow().date().strftime("%y%m%d") #
+    directory = os.path.join(os.path.dirname(__file__), "..", 'datas', 'DataLake', 'predictions', now_year) #
 
-    directory = os.path.join(os.path.dirname(__file__), "..", 'datas', 'DataLake', 'predictions')
-    with open("%s/%s/%s_predictions.json" % (directory, now_date, now_date), "r") as json_file:
+    with open("%s/%s_predictions.json" % (directory, now_date), "r") as json_file:
         data = json.load(json_file)
 
     data['data'].append(tmp_data)
 
     # print(tmp_data)
 
-    with open("%s/%s/%s_predictions.json" % (directory, now_date, now_date), "w") as json_file:
+    with open("%s/%s_predictions.json" % (directory, now_date), "w") as json_file:
         json.dump(data, json_file, indent=4)
-        print("sucksex")
+        print("Load is done!")
